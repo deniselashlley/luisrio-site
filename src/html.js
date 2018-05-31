d65d7f7e707626aea3223248937b692b45d5f2cc
teresachao <38939737+teresachao@users.noreply.github.com>
2018-05-31 15:50:15 +0100

    Add files via upload

---
+++ src/html.js
@@ -0,0 +1,56 @@
+import React from "react"
+
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    const buildDirectory = process.env.GATSBY_BUILD_DIR || `public`
+    stylesStr = require(`!raw-loader!../${buildDirectory}/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export default class HTML extends React.Component {
+  render() {
+    let css
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          key="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      )
+    }
+
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          {this.props.headComponents}
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          />
+        
+          <meta name="msapplication-config" content={`/browserconfig.xml`} />
+          <script src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js" />
+
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
+          />
+        </body>
+      </html>
+    )
+  }
+}
