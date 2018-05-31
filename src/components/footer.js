d2fcba288822e33ab6d9687bc3bf773503bfc780
teresachao <38939737+teresachao@users.noreply.github.com>
2018-05-31 16:05:19 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,11 @@ const Footer = () => (
     <footer className="footer" role="contentinfo">
         <ul className="social-network">
             <li>
-                <a href="https://www.facebook.com/luisrio.naturespecialist/" title="me siga on facebook">
+                <a href="https://www.facebook.com/luisrio.naturespecialist/" target="_blank" title="Siga-me no Facebook">
                 <i className="icon icon-fb"></i></a>
             </li>
             <li>
-              <a href="https://www.instagram.com/luisrio.jardins/" title="me siga instgram">
+              <a href="https://www.instagram.com/luisrio.jardins/" target="_blank" title="Siga-me no instagram">
               <i className="icon icon-instagram"></i>
               </a>
             </li>
@@ -18,59 +18,3 @@ const Footer = () => (
   )
   
   export default Footer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
