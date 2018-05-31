3dcf1fa5a4d4e3cc3e0d7687339f0d21f270cc3e
teresachao <38939737+teresachao@users.noreply.github.com>
2018-05-31 17:54:35 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,21 @@
 import React from 'react'
 
+const Index = () => (
+    <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Luis Rio - nature specialist' },
+        { name: 'keywords', content: 'Luis Rio, jardim, rega, manutenção, piscinas' },
+      ]}
+      link = {[{ rel: 'shortcut icon', type: 'image/x-icon', href:'/favicon.ico' }]}
+    />
+    <main role="main">
+      <Header />
+      <Contact />
+      <Footer />
+    </main>
+  </div>
+)
+
+export default Index
