30a256c7462209284edbb59071b6f9ef715b6951
teresachao <38939737+teresachao@users.noreply.github.com>
2018-05-31 18:36:06 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,11 @@ const Index = () => (
         { name: 'description', content: 'Luis Rio - nature specialist' },
         { name: 'keywords', content: 'Luis Rio, jardim, rega, manutenção, piscinas' },
       ]}
-      link = {[{ rel: 'shortcut icon', type: 'image/x-icon', href:'/favicon.ico' }]}
+      link = {[{ rel: 'shortcut icon', type: 'image/x-icon', href:'/favicon.ico' },
+    {
+        rel: "stylesheet",
+        href: "./public/styles.css"
+    }]}
     />
     <main role="main">
       <Header />
