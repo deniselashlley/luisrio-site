8b7d1206ed59791c5bc626076ee2dfe38d3eb213
teresachao <38939737+teresachao@users.noreply.github.com>
2018-05-31 18:32:07 +0100

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,7 +15,11 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Luis Rio - nature specialist' },
         { name: 'keywords', content: 'Luis Rio, jardim, rega, manutenção, piscinas' },
       ]}
-      link = {[{ rel: 'shortcut icon', type: 'image/x-icon', href:'/favicon.ico' }]}
+      link = {[{ rel: 'shortcut icon', type: 'image/x-icon', href:'/favicon.ico' },
+    {
+        rel: "stylesheet",
+        href: "./public/styles.css"
+    }]}
     />
     <main role="main">
       <Header />
